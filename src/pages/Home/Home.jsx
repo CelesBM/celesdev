1ad426019f73ed246bd1caed479c38f87a249994
unknown <celesbmontero@hotmail.com>
2024-03-19 20:51:31 -0300

    hero

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Hero from "../../components/Hero/Hero";
 import Footer from "../../components/Footer/Footer";
 
 const Home = () => {
   return (
     <>
+      <Hero />
       <Footer />
     </>
   );
