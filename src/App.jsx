7477255c4d058e2e005735f94d827af0f9ffaa29
unknown <celesbmontero@hotmail.com>
2024-03-19 20:33:25 -0300

    header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from "react";
 import { GlobalStyles } from "./GlobalStyles/GlobalStyles";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
+      <Header />
+      <Footer />
       <GlobalStyles />
     </>
   );
