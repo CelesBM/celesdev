53e5ad94a84457c101e6ebb6cc884f4fb556dff8
unknown <celesbmontero@hotmail.com>
2024-03-19 21:11:38 -0300

    projects

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home/Home";
+import Projects from "../pages/Projects/Projects";
 
 function Router() {
   return (
@@ -7,6 +8,7 @@ function Router() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Home" element={<Home />} />
+        <Route path="/Projects" element={<Projects />} />
       </Routes>
     </BrowserRouter>
   );
