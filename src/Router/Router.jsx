3b06326df69ebca1fdb6102eb44084d7c34a15dd
unknown <celesbmontero@hotmail.com>
2024-03-19 21:15:47 -0300

    social

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import Projects from "../pages/Projects/Projects";
+import Social from "../pages/Social/Social";
 
 function Router() {
   return (
@@ -9,6 +10,7 @@ function Router() {
         <Route path="/" element={<Home />} />
         <Route path="/Home" element={<Home />} />
         <Route path="/Projects" element={<Projects />} />
+        <Route path="/Social" element={<Social />} />
       </Routes>
     </BrowserRouter>
   );
